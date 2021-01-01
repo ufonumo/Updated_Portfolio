016aac84434190cafd9ad4e5c417d09eaf10e133
Ufonabasi Umo <31745515+ufonumo@users.noreply.github.com>
2021-01-01 02:42:47 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,23 @@
+AOS.init();
+
+$(document).ready(function(){
+
+    $(".filter-link").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+
+});
